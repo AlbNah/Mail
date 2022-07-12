880d02af11756393576d35b8ce499bc300683156
AlbNah <albertnahapetyanjs@gmail.com>
2022-07-12 13:26:06 +0400

    React app without unnecessary files and imports

---
+++ mail/src/App.js
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+      return(
+        <></>
+      )
+  }
+}
+
+
+export default App;
